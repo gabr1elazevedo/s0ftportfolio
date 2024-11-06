e42a1972a56f693b8a2a35a36bfb5ca7c7b07d6b
Gabriel Azevedo <gabrielaffonsoazevedo0@gmail.com>
2024-11-06 09:10:49 -0300

    Update BlogsImg.js

--- src/pages/contact/BlogsImg.js
+++ src/pages/contact/BlogsImg.js
@@ -5,7 +5,7 @@ export default class BlogsImg extends Component {
     const theme = this.props.theme;
     return (
       <svg
-        width={1300}
+        width={1100}
         height={700}
         viewBox="0 0 1061 742.85506"
         fill="none"
